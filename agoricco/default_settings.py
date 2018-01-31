8d7c3a88e8fdc425821b321bdfc5e67be373f1fc
Gregory Luneau <gregory.luneau@gmail.com>
2018-01-31 05:46:04 -0500

    re-setting english as default

--- agoricco/default_settings.py
+++ agoricco/default_settings.py
@@ -2,4 +2,4 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'supersecretdevkey'
-BABEL_DEFAULT_LOCALE = 'fr'
+BABEL_DEFAULT_LOCALE = 'en' # 'en' or 'fr'
